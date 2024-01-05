3d90189e642e3f188a9998c3bd121790f813a0c7
Sebastian Törnquist <sebb.tornquist@gmail.com>
2024-01-05 17:32:36 +0100

    fix: type annotations for options

--- lua/linter.lua
+++ lua/linter.lua
@@ -1,32 +1,74 @@
 local M = {}
 
-M.setup = function(opts)
-    local status, lspconfig = pcall(require, "lspconfig")
-    if not status then
-        print("lspconfig not found")
-    end
+---@alias ListOptions "quickfix"|"trouble"
 
-    if not lspconfig["tsserver"] then
-        print("make sure tsserver is installed and setup before loading tsserver-workspace-lint")
-    else
-        lspconfig["tsserver"].setup({
-            on_attach = function(client, bufnr)
-                local original = lspconfig["tsserver"].manager.config.capabilities.on_attach
-                vim.api.nvim_buf_create_user_command(0, "Tsc", function()
-                    local workspace = vim.lsp.get_active_clients({ bufnr = 0, name = "tsserver" })[1].config.root_dir
-                    vim.api.nvim_command(":compiler tsc | setlocal makeprg=tsc\\ -p\\ " .. workspace)
-                    vim.api.nvim_command(":make")
-                    local len = vim.api.nvim_exec2("echo len(getqflist())", { output = true }).output
-                    if tonumber(len) > 0 then
-                        vim.api.nvim_command(":copen")
-                    else
-                        print("No typescript errors found")
-                    end
-                end, { nargs = 0 })
-                original(client, bufnr)
-            end,
-        })
-    end
+---@class PluginOptions
+---@field auto_open? boolean
+---@field list? ListOptions
+
+---@type PluginOptions
+local default_options = {
+	auto_open = true,
+	list = "quickfix",
+}
+
+---@param opts PluginOptions
+M.setup = function(opts)
+	local status, lspconfig = pcall(require, "lspconfig")
+	if not status then
+		print("lspconfig not found")
+	end
+
+	if not lspconfig["tsserver"] then
+		print("make sure tsserver is installed and setup before loading tsserver-workspace-lint")
+	else
+		local options = vim.tbl_deep_extend("force", default_options, opts or {})
+
+		lspconfig["tsserver"].setup({
+			on_attach = function(client, bufnr)
+				local original = lspconfig["tsserver"].manager.config.capabilities.on_attach
+				vim.api.nvim_buf_create_user_command(0, "Tsc", function()
+					local workspaceRoot =
+						vim.lsp.get_active_clients({ bufnr = 0, name = "tsserver" })[1].config.root_dir
+
+					-- since tsc gives relative paths, we need to change the cwd to the workspace root
+					local oldCwd = vim.fn.getcwd()
+					vim.fn.chdir(workspaceRoot)
+
+					vim.api.nvim_command(":compiler tsc | setlocal makeprg=npx\\ tsc")
+					vim.api.nvim_command(":make")
+
+					-- change back to old cwd
+					vim.fn.chdir(oldCwd)
+
+					if not options.auto_open then
+						return
+					end
+
+					local quckfixEntries = vim.fn.getqflist()
+					if #quckfixEntries == 0 then
+						print("No typescript errors found")
+						return
+					end
+
+					for i = #quckfixEntries, 1, -1 do
+						if not quckfixEntries[i].valid then
+							table.remove(quckfixEntries, i)
+						end
+					end
+
+					if options.list == "quickfix" then
+						vim.api.nvim_command(":copen")
+					end
+
+					if options.list == "trouble" and require("trouble") then
+						require("trouble").open({ mode = "quickfix" })
+					end
+				end, { nargs = 0 })
+				original(client, bufnr)
+			end,
+		})
+	end
 end
 
 return M
